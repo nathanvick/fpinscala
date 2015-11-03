0e5e5859b2798f7b08398c4c25c8425bb31e8553
Nathan Vick <n.vick@samsung.com>
2015-11-03 14:55:50 -0800

    Completed exercises 6.10 through 6.11

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -36,15 +36,16 @@ object RNG {
 
   // Exercise 6.1:
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (i, rng1) = rng.nextInt
-    if (i > 0)
-      (i, rng1)
-    else if (i > Int.MinValue)
-      (-i, rng1)
-    else
-      (0, rng1)
+    //val (i, rng1) = rng.nextInt
+    //if (i < 0) (-i, rng1) else (i, rng1)
+    
+    map(int)(i => if (i < 0) -i else i)(rng)
   } 
 
+  def nonNegativeIntRand: Rand[Int] = {
+    map(int)(i => if (i < 0) -i else i)
+  } 
+  
   // Exercise 6.2:
   def double(rng: RNG): (Double, RNG) = {
     //val (i, rng1) = nonNegativeInt(rng)
@@ -104,38 +105,40 @@ object RNG {
   
   // Exercise 6.6:
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
-    //rng => {
-    //  val (a, rng1) = ra(rng)
-    //  val (b, rng2) = rb(rng1)
-    //  (f(a, b), rng2)
-    //}
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a, b), rng2)
+    }
     
     // Exercise 6.9:
-    flatMap(ra)(a => map(rb)(b => f(a, b)))    
+    //flatMap(ra)(a => map(rb)(b => f(a, b)))
   }
   
   def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
 
   // Exercise 6.7:
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
-    def go(fs: List[Rand[A]], rlist: Rand[List[A]]): Rand[List[A]] = {
-        if (fs == Nil)
-          rlist
-        else
-          go(fs.tail, map2(fs.head, rlist)((a, list) => a :: list))
-    }
+    //def go(fs: List[Rand[A]], acc: Rand[List[A]]): Rand[List[A]] = {
+    //    if (fs == Nil)
+    //      acc
+    //    else
+    //      go(fs.tail, map2(acc, fs.head)((list, a) => a :: list))
+    //}    
+    //val acc = go(fs, unit(List[A]()))
+    //map(acc)(list => list.reverse)
     
-    // The last A is added to the head of the list, but we want it at the end:
-    val rlist = go(fs, unit(List[A]()))
-    map(rlist)(list => list.reverse)
+    //val acc = fs.foldLeft(unit(List[A]()))((acc, ra) => map2(acc, ra)((list, a) => a :: list))
+    //map(acc)(list => list.reverse)    
+    
+    fs.foldRight(unit(List[A]()))((ra, acc) => map2(ra, acc)(_ :: _))
   }
   
   // Exercise 6.8:
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
     rng => {
       val (a, rng1) = f(rng)
-      val (b, rng2) = g(a)(rng1)
-      (b, rng2)
+      g(a)(rng1)
     }
   }
   
@@ -148,12 +151,34 @@ object RNG {
 }
 
 case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+  // Exercise 6.10:
+  def map[B](f: A => B): State[S, B] = {
+    //State(s => {
+    //  val (a, s1) = run(s)
+    //  (f(a), s1)
+    //})
+    
+    flatMap(a => State.unit((f(a))))
+  }
+  
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+    //State(s => {
+    //  val (a, s1) = run(s)
+    //  val (b, s2) = sb.run(s1)
+    //  (f(a, b), s2)
+    //})
+    
+    //flatMap(a => sb.map(b => f(a, b)))
+    
+    for (a <- this; b <- sb) yield (f(a, b))
+  }
+  
+  def flatMap[B](f: A => State[S, B]): State[S, B] = {
+    State(s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    })
+  }
 }
 
 sealed trait Input
@@ -164,5 +189,61 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  
+  // Exercise 6.11:
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    if (inputs.isEmpty) {
+      State(m => extractState(m))
+    } else {
+      val stateTransitions = inputs.map(evaluateIntput)
+      sequence(stateTransitions).map(results => results.last)
+    }
+  }
+
+  def evaluateIntput(in: Input): State[Machine, (Int, Int)] = {
+    State{m => (in, m) match {
+        case (Coin, m @ Machine(_,     candies, _)) if candies > 0 => extractState(Machine(false, m.candies,     m.coins + 1))
+        case (Turn, m @ Machine(false, candies, _)) if candies > 0 => extractState(Machine(true,  m.candies - 1, m.coins))
+        case (_, m) => extractState(m)
+      }
+    }    
+  }
+  
+  def extractState(m: Machine): ((Int, Int), Machine) = ((m.coins, m.candies), m)
+  
+  // Exercise 6.10:
+  def unit[S,A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[S,A](fs: List[State[S,A]]): State[S, List[A]] = {
+    //def go(fs: List[State[S,A]], acc: State[S, List[A]]): State[S, List[A]] = {
+    //    if (fs == Nil)
+    //      acc
+    //    else
+    //      go(fs.tail, acc.map2(fs.head)((list, a) => a :: list))
+    //}    
+    //val acc = go(fs, unit(List[A]()))
+    //acc.map(list => list.reverse)
+    
+    fs.foldRight(unit[S,List[A]](List[A]()))((sa, acc) => sa.map2(acc)(_ :: _))
+  }    
+
+  // Exercise 6.10 (extra mile):
+  val _int: Rand[Int] = State(_.nextInt)
+
+  def _nonNegativeInt: Rand[Int] = {
+    _int.map {i =>
+      if (i > 0)
+        i
+      else if (i > Int.MinValue)
+        -i
+      else
+        0
+    }
+  } 
+
+  def _ints(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(_int))
+  }
+  
 }
--- exercises/src/test/scala/fpinscala/state/StateSpec.scala
+++ exercises/src/test/scala/fpinscala/state/StateSpec.scala
@@ -34,14 +34,107 @@ class StateSpec extends FlatSpec with Matchers {
   
   // Exercise 6.4:
   "ints" should "return a list of ints" in {
-    ints(3)(Simple(-5581))._1 shouldBe List(1154945163, 2017739370, -2147283612)
-    ints(3)(Simple(42))._1 shouldBe List(-340305902, -1281479697, 16159453)
-  }  
+    ints(3)(Simple(-5581))._1 shouldBe List(int(Simple(-5581))._1, int(int(Simple(-5581))._2)._1, int(int(int(Simple(-5581))._2)._2)._1)
+    ints(3)(Simple(-5581))._1 shouldBe List(-2147283612, 2017739370, 1154945163)
+    ints(3)(Simple(42))._1 shouldBe List(16159453, -1281479697, -340305902)
+  }
+  
+  // Exercise 6.6:
+  "both" should "execute two state transformations in sequence" in {
+    both(int, int)(Simple(-5581))._1 shouldBe (-2147283612, 2017739370)
+    both(int, int)(Simple(42))._1 shouldBe (16159453, -1281479697)
+
+    both(int, nonNegativeIntRand)(Simple(-5581))._1 shouldBe (int(Simple(-5581))._1, nonNegativeIntRand(int(Simple(-5581))._2)._1)
+    both(int, nonNegativeIntRand)(Simple(42))._1 shouldBe (int(Simple(42))._1, nonNegativeIntRand(int(Simple(42))._2)._1)
+    
+    both(int, nonNegativeIntRand)(Simple(-5581))._1 shouldBe (-2147283612, 2017739370)
+    both(int, nonNegativeIntRand)(Simple(42))._1 shouldBe (16159453, 1281479697)
+  }
+  
+  // Exercise 6.7:
+  "sequence" should "execute all state transformations in sequence" in {
+    RNG.sequence(List(int, nonNegativeIntRand))(Simple(-5581))._1 shouldBe List(-2147283612, 2017739370)
+    RNG.sequence(List(int, nonNegativeIntRand))(Simple(42))._1 shouldBe List(16159453, 1281479697)
+  }
 
   // Exercise 6.8:
   "nonNegatgiveLessThan" should "return an int in the specified range" in {
     nonNegatgiveLessThan(10)(Simple(-5581))._1 shouldBe 2
     nonNegatgiveLessThan(10)(Simple(42))._1 shouldBe 3
+  }
+
+  // Exercise 6.10 (extra mile):
+  "_nonNegativeInt" should "return an integer from 0 to Integer.MaxValue, inclusive" in {
+    _nonNegativeInt.run(Simple(-5581))._1 shouldBe 2147283612
+    _nonNegativeInt.run(Simple(42))._1 shouldBe 16159453
+  }
+
+  "_ints" should "return a list of ints" in {
+    _ints(3).run(Simple(-5581))._1 shouldBe List(-2147283612, 2017739370, 1154945163)
+    _ints(3).run(Simple(42))._1 shouldBe List(16159453, -1281479697, -340305902)
+  }  
+
+  "simulateMachine" should "be unaffected by empty input" in {
+    val ((coins, candies), machine) = simulateMachine(List()).run(Machine(true, 7, 0))
+    coins shouldBe 0
+    candies shouldBe 7
+    machine.locked shouldBe true
+  }
+  
+  it should "unlock when it receives a coin" in {
+    val ((coins, candies), machine) = simulateMachine(List(Coin)).run(Machine(true, 7, 0))
+    coins shouldBe 1
+    candies shouldBe 7
+    machine.locked shouldBe false
+  }
+  
+  it should "stay unlocked when it receives a coin" in {
+    val ((coins, candies), machine) = simulateMachine(List(Coin)).run(Machine(false, 7, 0))
+    coins shouldBe 1
+    candies shouldBe 7
+    machine.locked shouldBe false
+  }
+
+  it should "lock when it dispenses a candy" in {
+    val ((coins, candies), machine) = simulateMachine(List(Turn)).run(Machine(false, 7, 1))
+    coins shouldBe 1
+    candies shouldBe 6
+    machine.locked shouldBe true
+  }
+
+  it should "ignore turns when it is locked" in {
+    val ((coins, candies), machine) = simulateMachine(List(Turn)).run(Machine(true, 7, 0))
+    coins shouldBe 0
+    candies shouldBe 7
+    machine.locked shouldBe true
+  }
+  
+  it should "ignore turns when it is out of candy and locked" in {
+    val ((coins, candies), machine) = simulateMachine(List(Turn)).run(Machine(true, 0, 99))
+    coins shouldBe 99
+    candies shouldBe 0
+    machine.locked shouldBe true
+  }
+
+  it should "ignore turns when it is out of candy and unlocked" in {
+    val ((coins, candies), machine) = simulateMachine(List(Turn)).run(Machine(false, 0, 99))
+    coins shouldBe 99
+    candies shouldBe 0
+    machine.locked shouldBe false
+  }
+  
+  it should "ignore coins when it is out of candy and locked" in {
+    val ((coins, candies), machine) = simulateMachine(List(Coin)).run(Machine(true, 0, 99))
+    coins shouldBe 99
+    candies shouldBe 0
+    machine.locked shouldBe true
+  }
+
+  it should "ignore coins when it is out of candy and unlocked" in {
+    val ((coins, candies), machine) = simulateMachine(List(Coin)).run(Machine(false, 0, 99))
+    coins shouldBe 99
+    candies shouldBe 0
+    machine.locked shouldBe false
   }
   
 }
